6fe56c625f16c412915dfc6f3fd4dcc7ab9dd3a9
prapun <39039851+prapun77@users.noreply.github.com>
2018-06-15 10:25:40 +0700

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -110,7 +110,7 @@ public:
         genesis.nTime    = 1528959903 ;
         genesis.nBits    = bnProofOfWorkLimit.GetCompact(); 
         genesis.nNonce   = 37257;
-        hashGenesisBlock = genesis.GetHash();
+        //hashGenesisBlock = genesis.GetHash();
         // Mine the genesis block.
         if (true)
         {
@@ -125,7 +125,7 @@ public:
             LogPrintf("new mainnet genesis hash: %s\n", genesis.GetHash().ToString().c_str());
         }
 
-       // hashGenesisBlock = genesis.GetHash();
+        hashGenesisBlock = genesis.GetHash();
 
         assert(hashGenesisBlock == uint256("0x0000f95359bf62a4f900903f8fa14827df2cf1d91142ee08fd2b17f1db3d155b"));
         assert(genesis.hashMerkleRoot == uint256("0x8df429a49645fa23294a6da824f88c0c6ebde633b282f4e81b66e98f3be4c93d"));

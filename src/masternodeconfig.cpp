8518c8691ba2e78847f2648a1ff06a43b4a6bd41
prapun77 <prapun77@gmail.com>
2018-06-16 20:41:25 +0700

    change port for masternode

--- src/masternodeconfig.cpp
+++ src/masternodeconfig.cpp
@@ -52,13 +52,13 @@ bool CMasternodeConfig::read(boost::filesystem::path path) {
         }
 
         if(Params().NetworkID() == CChainParams::MAIN){
-            if(CService(ip).GetPort() != 17177) {
-                LogPrintf("Invalid port detected in masternode.conf: %s (must be 17177 for mainnet)\n", line.c_str());
+            if(CService(ip).GetPort() != 17711) {
+                LogPrintf("Invalid port detected in masternode.conf: %s (must be 17711 for mainnet)\n", line.c_str());
                 streamConfig.close();
                 return false;
             }
-        } else if(CService(ip).GetPort() == 17177) {
-            LogPrintf("Invalid port detected in masternode.conf: %s (17177 must be only on mainnet)\n", line.c_str());
+        } else if(CService(ip).GetPort() == 17711) {
+            LogPrintf("Invalid port detected in masternode.conf: %s (17711 must be only on mainnet)\n", line.c_str());
             streamConfig.close();
             return false;
         }
--- src/masternodeman.cpp
+++ src/masternodeman.cpp
@@ -808,9 +808,9 @@ void CMasternodeMan::ProcessMessage(CNode* pfrom, std::string& strCommand, CData
         }
 
         if(Params().NetworkID() == CChainParams::MAIN){
-            if(addr.GetPort() != 17177) return;
+            if(addr.GetPort() != 17711) return;
             if(!isIPV4) return;
-        } else if(addr.GetPort() == 17177) return;
+        } else if(addr.GetPort() == 17711) return;
 
         //search existing masternode list, this is where we update existing masternodes with new dsee broadcasts
         CMasternode* pmn = this->Find(vin);
